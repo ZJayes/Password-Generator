9c85c0ad87449f24def5c30539df68dede65daa3
Zachary Jayes <zacjayes@gmail.com>
2020-11-21 20:50:32 -0600

    added if statements

--- script.js
+++ script.js
@@ -18,20 +18,45 @@ function generatePassword() {
   var number = confirm("Do you want special characters?");
   var special = confirm("Do you want to add numbers?")
 
-
-
+ 
 var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
 var number = ["0","1","2","3","4","5","6","7","8","9"];
   
-var passwordLength = prompt ("Please chose a length for your password between 8 and 128.");
+var passwordLength = prompt ("Please choose a length for your password between 8 and 128.");
 if ((passwordLength < 8) || (userLength > 128)){
-  alert("Please pick a password length.");
+  alert("Please pick a password length between 8 and 128.");
   passwordLength = prompt("How long do you want your password?")
 }
 
+if ((!lower) && (!upper) && (!number) && (!special)){
+  alert("Please select a correct prompts.");
+}
 
+if(lower){
+  for(var i=0; i < lowerCase.length; i++){
+    choice.push(lowerCase[i]);
+  }
+}
+if (upper) {
+  for(var i = 0; i < upperCase.length; i++ ){
+    choice.push(upperCase[i]);
+
+  }
+}
+
+if(number){
+  for(var i=0; i < number.length; i++){
+    choice.push(number[i]);
+  }
+}
+
+if(special){
+  for(var i=0; i < special.length; i++){
+    choice.push(special[i]);
+  }
+}
 
 
 
