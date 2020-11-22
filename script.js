080e6b27cb8c784b62d1a245637980c4e80cb5d1
Zachary Jayes <zacjayes@gmail.com>
2020-11-21 20:23:30 -0600

    adding password length

--- script.js
+++ script.js
@@ -13,19 +13,23 @@ function writePassword() {
 function generatePassword() {
 
   var choice = [];
+  var lower = confirm("Insert lowercase letters?");
+  var upper = confirm("Insert uppercase letters?");
+  var number = confirm("Insert numbers?");
+  var special = confirm("Insert special characters?");
 
-var upper = confirm("Do you want capital letters?");
-var lower = confirm("Do you want lowercase letters ?");
-var special = confirm("Do you want special characters?");
-var number = confirm("Do you want to add numbers?");
+
+
+var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+var number = ["0","1","2","3","4","5","6","7","8","9"];
   
-
-  var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var special = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
-  var number = ["0","1","2","3","4","5","6","7","8","9"];
-  
-
+var passwordLength = prompt ("Please chose a length for your password between 8 and 128.");
+if ((passwordLength < 8) || (userLength > 128)){
+  alert("Please pick a password length.");
+  passwordLength = prompt("How long do you want your password?")
+}
 
 
 
