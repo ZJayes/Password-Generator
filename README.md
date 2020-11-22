cb519ddd7395679c00b7cc7066be536c81dbb10d
Zachary Jayes <zacjayes@gmail.com>
2020-11-21 21:42:28 -0600

    add readme + final of page

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Password-Generator
+I created a password generator.  This password generator is able to make the user create a random password using uppercase letters, lowercase letters, special characters, and numbers.  This generator also allows you to select the amount of characters that you use between 8-128.  
+
+Directions:
+-First, you select the "Generate Password" button
+-Second, a prompt to select lowercase letters appears.  The user selects ok or cancel.
+-Third, a prompt to select uppercase letters appears.
+The user selects ok or cancel.
+-Fourth,a prompt to select special characters appears.
+The user selects ok or cancel.
+-Fifth,a prompt to select numbers appears.
+The user selects ok or cancel.
+-Sixth, a prompt to select the amount of characters between 8-128.
+The use selects their choice in the message bar.
+-Finally, the screen will display the new generated password.
+
+Below is the screenshot of my page:
+
+![image](https://user-images.githubusercontent.com/71290304/99893154-35fa6c80-2c42-11eb-834d-ef98d938fb06.png)
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -16,7 +17,14 @@ function generatePassword() {
   var lower = confirm("Do you want lowercase letters?");
   var upper = confirm("Do you want capital letters?");
   var number = confirm("Do you want special characters?");
-  var special = confirm("Do you want to add numbers?")
+  var special = confirm("Do you want to add numbers?");
+
+  if((!upper) && (!lower) && (!special) && (!number)) {
+    alert ("Please choose a prompt:");
+    
+  }
+  
+    
 
  
 var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -25,23 +33,23 @@ var special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=","
 var number = ["0","1","2","3","4","5","6","7","8","9"];
   
 var passwordLength = prompt ("Please choose a length for your password between 8 and 128.");
-if ((passwordLength < 8) || (userLength > 128)){
+if ((passwordLength < 8) || (passwordLength > 128)){
   alert("Please pick a password length between 8 and 128.");
   passwordLength = prompt("How long do you want your password?")
 }
 
 if ((!lower) && (!upper) && (!number) && (!special)){
-  alert("Please select a correct prompts.");
+  alert("Please select the correct prompt.");
 }
 
 if(lower){
-  for(var i=0; i < lowerCase.length; i++){
-    choice.push(lowerCase[i]);
+  for(var i=0; i < lowerLetter.length; i++){
+    choice.push(lowerLetter[i]);
   }
 }
 if (upper) {
-  for(var i = 0; i < upperCase.length; i++ ){
-    choice.push(upperCase[i]);
+  for(var i = 0; i < upperLetter.length; i++ ){
+    choice.push(upperLetter[i]);
 
   }
 }
@@ -57,22 +65,16 @@ if(special){
     choice.push(special[i]);
   }
 }
+var newPassword = [];
+for(i=0; i < passwordLength; i++){
+  newPassword.push(choice[Math.floor(Math.random()*choice.length)]);
+}
 
-
-
+return(newPassword.join(""));
 
 
 
 }
-
-
-
-
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
